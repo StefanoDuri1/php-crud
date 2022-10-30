6edd56c51be303cd3a549cee9595b081a63a7f06
Stefano Duri <duristefano@gmail.com>
2022-10-30 18:56:00 -0300

    Adding clients to the list

--- index.php
+++ index.php
@@ -13,6 +13,29 @@
         </div>
         <div class="col-md-6">
             <h3 class="mb-4">Your Clients</h3>
+            <ul class="list-group">
+                <?php
+
+                    $query = "SELECT * FROM clients";
+                    $clientsData = mysqli_query($connection, $query);
+
+                    while($row = mysqli_fetch_array($clientsData)) { ?>
+                        <div class"info p-3">
+                            <h2 class="card-title fw-bolder"> <?php echo $row['client_name'];?> </h2>
+                            <p>
+                                <span class="fw-bolder">Cellphone: </span>
+                                <?php echo $row['cellphone']; ?>
+                            </p>
+                            <p>
+                                <span class="fw-bolder">Email: </span><?php echo $row['email']; ?>
+                            </p>
+                            <p>
+                                <span class="fw-bolder">Description: </span>
+                                <?php echo $row['description']; ?>
+                            </p>
+                        </div>
+                <?php } ?>
+            </ul>
         </div>
     </div>
 </div>
