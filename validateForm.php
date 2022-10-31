00613c9748d33d55b344a5361de2661bd1e8c1d7
Stefano Duri <duristefano@gmail.com>
2022-10-31 12:04:22 -0300

    Solving problems

--- validateForm.php
+++ validateForm.php
@@ -8,28 +8,29 @@
         $email = $_POST['email'];
         $description = $_POST['description'];
 
-        $query = "INSERT INTO clients(client_name, cellphone, email, description) VALUES('$client_name', '$cellphone', '$email', '$description')";
-        $data = mysqli_query($connection, $query);
+        if(!empty($client_name) && !empty($cellphone) && !empty($email) && !empty($description)) {
+            $query = "INSERT INTO clients(client_name, cellphone, email, description) VALUES('$client_name', '$cellphone', '$email', '$description')";
+            $data = mysqli_query($connection, $query);
 
-        if(!$data) {
-            die('Query failed');
-        }
-
-        $_SESSION['labels'] = array();
-
-        if(empty($client_name)) {
-            $_SESSION['labels'][] = "The name can't be empty";
-        }
-        if(empty($cellphone) || !is_numeric($cellphone)) {
-            $_SESSION['labels'][] = "Enter a valid cellphone number";
-        }
-        if(empty($email) || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            $_SESSION['labels'][] = "Enter a valid email";
-        }
-        if(empty($description)) {
-            $_SESSION['labels'][] = "Enter a description";
-        }
-
+            if(!$data) {
+                die('Query failed');
+            }
+            $_SESSION['labels'] = array();
+        }else {
+            if(empty($client_name)) {
+                $_SESSION['labels'][] = "The name can't be empty";
+            }
+            if(empty($cellphone) || !is_numeric($cellphone)) {
+                $_SESSION['labels'][] = "Enter a valid cellphone number";
+            }
+            if(empty($email) || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                $_SESSION['labels'][] = "Enter a valid email";
+            }
+            if(empty($description)) {
+                $_SESSION['labels'][] = "Enter a description";
+            }
+            }
+        
 
         if(count($_SESSION['labels']) > 0) {
             echo "<div class='alert alert-danger mx-3 list-unstyled'>";
